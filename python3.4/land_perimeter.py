247d40ef637707eb9bd8f5f622f636e488c9d099
demvy <demes661@gmail.com>
2017-03-11 15:14:57 +0200

    need to fix weird_gen

--- python3.4/land_perimeter.py
+++ python3.4/land_perimeter.py
@@ -30,12 +30,32 @@ Good luck!
 
 
 def land_perimeter(arr):
-    return #perimeter of all the islands
+    s = 0
+    for i, line in enumerate(arr):
+        for j, el in enumerate(line):
+            if el == 'X' and j == 0:
+                s += 1
+            if el == 'X' and j == len(line) - 1:
+                s += 1
+            if i == 0 and el == 'X':
+                s += 1
+            if i == len(arr) - 1 and el == 'X':
+                s += 1
+            if j < len(line) - 1 and el == 'X' and line[j + 1] == 'O':
+                s += 1
+            if i > 0 and el == 'X' and arr[i - 1][j] == 'O':
+                s += 1
+            if i < len(arr) - 1 and el == 'X' and arr[i + 1][j] == "O":
+                s += 1
+            if j > 0 and el == 'X' and line[j - 1] == 'O':
+                s += 1
+    return "Total land perimeter: %d" % s
 
 
 if __name__ == "__main__":
     print (land_perimeter(["OXOOOX", "OXOXOO", "XXOOOX", "OXXXOO", "OOXOOX", "OXOOOO", "OOXOOX", "OOXOOO", "OXOOOO", "OXOOXX"]) == "Total land perimeter: 60")
     print(land_perimeter(["OXOOO", "OOXXX", "OXXOO", "XOOOO", "XOOOO", "XXXOO", "XOXOO", "OOOXO", "OXOOX", "XOOOO", "OOOXO"]) == "Total land perimeter: 52")
-    print(land_perimeter(["XXXXXOOO", "OOXOOOOO", "OOOOOOXO", "XXXOOOXO", "OXOXXOOX"]) == "Total land perimeter: 40")
+    print(land_perimeter(["XXXXXOOO", "OOXOOOOO", "OOOOOOXO", "XXXOOOXO", "OXOXXOOX"])== "Total land perimeter: 40")
     print(land_perimeter(["XOOOXOO", "OXOOOOO", "XOXOXOO", "OXOXXOO", "OOOOOXX", "OOOXOXX", "XXXXOXO"]) == "Total land perimeter: 54")
     print(land_perimeter(["OOOOXO", "XOXOOX", "XXOXOX", "XOXOOO", "OOOOOO", "OOOXOO", "OOXXOO"]) == "Total land perimeter: 40")
+    print(land_perimeter(["OOXOO"]))
---
+++ python3.4/weird_prime_generator.py
@@ -0,0 +1,87 @@
+
+"""
+Consider the sequence a(1) = 7, a(n) = a(n-1) + gcd(n, a(n-1)) for n >= 2:
+
+7, 8, 9, 10, 15, 18, 19, 20, 21, 22, 33, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 69, 72, 73....
+
+Let us take the differences between successive elements of the sequence and get a second sequence g: 1, 1, 1, 5, 3, 1, 1, 1, 1, 11, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 23, 3, 1....
+
+For the sake of uniformity of the lengths of sequences we add a 1 at the head of g:
+
+g: 1, 1, 1, 1, 5, 3, 1, 1, 1, 1, 11, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 23, 3, 1...
+
+Removing the 1s gives a third sequence: p: 5, 3, 11, 3, 23, 3... where you can see prime numbers.
+
+Task:
+
+Write functions:
+
+1: an(n) with parameter n: returns the first n terms of the series a(n) (not tested)
+
+2: gn(n) with parameter n: returns the first n terms of the series g(n) (not tested)
+
+3: countOnes(n) with parameter n: returns the number of 1 in g(n)
+    (don't forget to add a `1` at the head) # (tested)
+
+4: p(n) with parameter n: returns an array of n unique prime numbers (not tested)
+
+5: maxp(n) with parameter n: returns the biggest prime number of the sequence pn(n) # (tested)
+
+6: anOver(n) with parameter n: returns an array (n terms) of the a(i)/i for every i such g(i) != 1 (not tested but interesting result)
+
+7: anOverAverage(n) with parameter n: returns as an *integer* the average of anOver(n)  (tested)
+Note:
+
+You can write directly functions 3:, 5: and 7:. There is no need to write functions 1:, 2:, 4: 6: except out of pure curiosity.
+"""
+from fractions import gcd
+from collections import Counter
+
+
+def is_prime(a):
+    return all(a % i for i in xrange(2, a))
+
+
+def a(n):
+    if n == 1:
+        return 7
+    return a(n-1) + gcd(n, a(n - 1))
+
+
+def an(n):
+    lst = [7]
+    for i in range(2, n + 1):
+        lst.append(a(n))
+    return lst
+
+
+def gn(n):
+    lst = [1]
+    a = an(n)
+    lst.extend([a[i] - a[i - 1] for i, b in enumerate(a) if i > 0])
+    return lst
+
+
+def count_ones(n):
+    return Counter(gn(n))[1]
+
+
+def p(n):
+    return [el for el in gn(n) if is_prime(el)]
+
+
+def max_pn(n):
+    return max(p(n))
+
+
+def an_over(n):
+    return [an(i)[-1]/i for i in range(2, n)]
+
+
+def an_over_average(n):
+    return sum(an_over(n)) / len(an_over(n))
+
+if __name__ == "__main__":
+    print(an(10))
+    print(count_ones(10)) #8
+    print(count_ones(100)) #90
