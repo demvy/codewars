68d63fbfe57e6abefeb106d5df79f4cf01912846
Valeriy Demeshko <vdemeshko@sequans.com>
2019-05-03 13:06:12 +0300

    vasya_cleark updated, read main()

--- cpp/vasya_clerk.cpp
+++ cpp/vasya_clerk.cpp
@@ -13,6 +13,48 @@ tickets({25, 100}) // => NO. Vasya will not have enough money to give change to 
 tickets({25, 25, 50, 50, 100}) // => NO. Vasya will not have the right bills to give 75 dollars of change (you can't make two bills of 25 from one of 50)
 */
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <string>
+
 std::string tickets(const std::vector<int> peopleInLine){
-  // your code here
+	if (peopleInLine.size())
+	{
+		std::vector<int> cash_box = {};
+		std::map<int, std::vector<int>> change_map = {{25, {0}}, {50, {25}}, {100, {25, 50}}};
+		std::vector<int>::iterator where;
+
+		for(auto it = peopleInLine.begin(); it < peopleInLine.end(); it++)
+		{
+			if (change_map[*it][0])
+			{
+				// Check if we have change for people's cache
+				for(auto j = change_map[*it].begin(); j < change_map[*it].end(); j++)
+				{
+					where = find(cash_box.begin(), cash_box.end(), *j);
+
+					if (where == cash_box.end())
+						return "NO";
+					else
+						cash_box.erase(where); // Delete change fron cash box if we have it
+				}
+				// If we have all change, add people's money to cash box
+				cash_box.push_back(*it);
+			}
+			else
+				cash_box.push_back(*it);
+		}
+	}
+	return "YES";
+}
+
+int main(void)
+{
+	//Should return YES, 25*3 can be change for 100
+	std::vector<int> a = {25,50,25,100,25,25,50,100,25,25,25,100,25,25,50,100,25,50,25,100,25,50,50,50};
+
+	std::cout << tickets(a) << std::endl;
+	return 0;
 }
